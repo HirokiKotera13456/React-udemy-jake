62120d2854cccca8dca0eacf14594b8835646a7b
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-12-09 15:55:34 +0900

    Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./page1DetailA";
+import { Page1DetailB } from "./page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -20,9 +22,22 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route exact path="/page1/detailA">
+                <Page1DetailA />
+              </Route>
+              <Route exact path="/page1/detailB">
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
@@ -30,5 +45,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
-//モックアップ
